d94791e4e1415077946b85010874adcbc1b96686
unshrawal <32817861+unshrawal@users.noreply.github.com>
2022-07-13 09:08:33 -0500

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,29 @@
+class Solution:
+    def my_grocery_list(self,str1,str2):
+        # type str1:string
+        # type str2: string
+        # return: list
+
+        # TODO: Write code below to return a list with the solution to the prompt
+        l1 = str1.split(" ")
+        l2 = str2.split(" ")
+        l3 = []
+        for i in l1:
+            if i not in l3:
+                l3.append(i)
+        for i in l2:
+            if i not in l3:
+                l3.append(i)
+        return l3
+        pass
+
+def main():
+    string1 = input()
+    string2 = input()
+
+    tc1 = Solution()
+    ans = tc1.my_grocery_list(string1,string2)
+    print(ans)
+
+if __name__ == "__main__":
+     main()
